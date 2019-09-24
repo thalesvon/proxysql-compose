a3ce4648a5c4a12ab8885c3cf78ebac9f0a2737b
Thales Sperling <thalesvon@gmail.com>
2019-09-24 17:25:52 -0300

    changing proxysql configs file for local and rds

--- proxy-sql/rds-sample-config.sql
+++ proxy-sql/rds-sample-config.sql
@@ -1,13 +1,15 @@
 DELETE FROM mysql_servers;
-INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag) VALUES (0,'write-sql',3306,1);
-INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag) VALUES (1,'read-sql',3307,1);
+INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag) VALUES (0,'{{ sql:writer-node }}',{{ sql:port }},1);
+INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag) VALUES (1,'{{ sql:reader-node }}',{{ sql:port }},1);
 DELETE FROM mysql_replication_hostgroups;
+-- for AWS Aurora check_type = innodb_read_only
 INSERT INTO mysql_replication_hostgroups (writer_hostgroup,reader_hostgroup,check_type) VALUES (0,1,'innodb_read_only');
+
 LOAD MYSQL SERVERS TO RUNTIME;
 SAVE MYSQL SERVERS TO DISK;
 
 DELETE FROM mysql_users;
-INSERT INTO mysql_users (username,password,active,default_hostgroup) values ('root','mysql',1,0);
+INSERT INTO mysql_users (username,password,active,default_hostgroup) values ('{{ sql:user }}','{{ sql:password }}',1,0);
 LOAD MYSQL USERS TO RUNTIME;
 SAVE MYSQL USERS TO DISK; 
 
@@ -16,8 +18,8 @@ INSERT INTO mysql_query_rules (rule_id,active,match_digest,destination_hostgroup
 LOAD MYSQL QUERY RULES TO RUNTIME;
 SAVE MYSQL QUERY RULES TO DISK;
 
-UPDATE global_variables SET variable_value='root' WHERE variable_name='mysql-monitor_username';
-UPDATE global_variables SET variable_value='mysql' WHERE variable_name='mysql-monitor_password';
+UPDATE global_variables SET variable_value='{{ sql:user }}' WHERE variable_name='mysql-monitor_username';
+UPDATE global_variables SET variable_value='{{ sql:password }}' WHERE variable_name='mysql-monitor_password';
 UPDATE global_variables SET variable_value='2000' WHERE variable_name IN ('mysql-monitor_connect_interval','mysql-monitor_ping_interval','mysql-monitor_read_only_interval');
 LOAD MYSQL VARIABLES TO RUNTIME;
 SAVE MYSQL VARIABLES TO DISK;
