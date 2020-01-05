3e1065047c033ebce573ed15650d3aff795ba8b8
von Sperling <sperlt@a483e70b9427.ant.amazon.com>
2020-01-05 00:41:02 +0000

    last changes

--- proxy-sql/local-config.sql
+++ proxy-sql/local-config.sql
@@ -1,8 +1,8 @@
 DELETE FROM mysql_servers;
-INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag) VALUES (0,'write-sql',3306,1);
-INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag) VALUES (1,'read-sql',3307,1);
+INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag) VALUES (0,'write-sql',3306,0);
+INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag) VALUES (1,'read-sql',3307,0);
 DELETE FROM mysql_replication_hostgroups;
-INSERT INTO mysql_replication_hostgroups (writer_hostgroup,reader_hostgroup,check_type) VALUES (0,1,'read_only');
+INSERT INTO mysql_replication_hostgroups (writer_hostgroup,reader_hostgroup,check_type) VALUES (0,1,'innodb_read_only');
 LOAD MYSQL SERVERS TO RUNTIME;
 SAVE MYSQL SERVERS TO DISK;
 
@@ -12,10 +12,13 @@ LOAD MYSQL USERS TO RUNTIME;
 SAVE MYSQL USERS TO DISK; 
 
 DELETE FROM mysql_query_rules;
-INSERT INTO mysql_query_rules (rule_id,active,match_digest,destination_hostgroup,apply) VALUES (1,1,'^SELECT.*FOR UPDATE',1,1),(2,1,'^SELECT',1,1);
 LOAD MYSQL QUERY RULES TO RUNTIME;
 SAVE MYSQL QUERY RULES TO DISK;
 
+UPDATE global_variables SET variable_value='true' WHERE variable_name='mysql-client_session_track_gtid';
+UPDATE global_variables SET variable_value='true' WHERE variable_name='admin-checksum_mysql_query_rules';
+UPDATE global_variables SET variable_value='true' WHERE variable_name='admin-checksum_mysql_users';
+UPDATE global_variables SET variable_value='true' WHERE variable_name='admin-checksum_mysql_servers';
 UPDATE global_variables SET variable_value='root' WHERE variable_name='mysql-monitor_username';
 UPDATE global_variables SET variable_value='mysql' WHERE variable_name='mysql-monitor_password';
 UPDATE global_variables SET variable_value='2000' WHERE variable_name IN ('mysql-monitor_connect_interval','mysql-monitor_ping_interval','mysql-monitor_read_only_interval');
--- sql/create-db.sql
+++ sql/create-db.sql
@@ -1,3 +1,4 @@
 create database `example-db`;
 use `example-db`;
 create table person (id int NOT NULL AUTO_INCREMENT,name varchar(255), primary key (id));
+insert into `example-db`.person ( name ) values ('jarbas');
